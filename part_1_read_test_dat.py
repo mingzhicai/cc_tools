637161f205c050e51865219e285c3384e0dd7214
Mingzhi Cai <minztsai96@gmail.com>
2020-02-12 19:39:31 -0500

    phase1_mingzhic

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -2,7 +2,7 @@ import cc_dat_utils
 
 #Part 1
 input_dat_file = "data/pfgd_test.dat"
-
-
 #Use cc_dat_utils.make_cc_level_pack_from_dat() to load the file specified by input_dat_file
+output_dat_file = cc_dat_utils.make_cc_level_pack_from_dat(input_dat_file)
 #print the resulting data
+print(output_dat_file)
